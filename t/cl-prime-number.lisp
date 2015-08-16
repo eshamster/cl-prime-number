a659596d1d439cf3cf807322b087936480fc5385
eshamster <hamgoostar@gmail.com>
2015-08-16 18:14:20 +0900

    Add the test of inverse-foctorize-from-prime
    	modified:   t/cl-prime-number.lisp

--- t/cl-prime-number.lisp
+++ t/cl-prime-number.lisp
@@ -8,7 +8,7 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-prime-number)' in your Lisp.
 
-(plan 2)
+(plan 3)
 
 (subtest
     "Test the series of prime"
@@ -24,5 +24,12 @@
   (is (factorize-in-prime 100) '(2 0 2) :test #'equalp)
   (is (factorize-in-prime 37) '(0 0 0 0 0 0 0 0 0 0 0 1) :test #'equal)
   (is (factorize-in-prime 171) '(0 2 0 0 0 0 0 1) :test #'equalp))
+
+(subtest
+    "Test inverse-factorize-from-prime"
+  (dolist (x '(1 4 72 100 37 171))
+    (is (inverse-factorize-from-prime
+	 (factorize-in-prime x))
+	x)))
 
 (finalize)
